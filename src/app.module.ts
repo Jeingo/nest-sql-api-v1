f3033a72470f41a8a16d8680342f18f88bf13852
Renat <jeingo20@gmail.com>
2023-02-25 13:43:25 +0300

    add ssl key

--- src/app.module.ts
+++ src/app.module.ts
@@ -211,7 +211,7 @@ const controllers = [
       username: process.env.PG_USERNAME,
       password: process.env.PG_PASSWORD,
       database: process.env.PG_DB_NAME,
-      // ssl: process.env.PG_SSL == 'true',
+      ssl: process.env.PG_SSL == 'true',
       autoLoadEntities: false,
       synchronize: false
     }),
