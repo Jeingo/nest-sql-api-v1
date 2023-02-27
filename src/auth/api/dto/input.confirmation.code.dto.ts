78ce9af41c9d73931eefa5d86f6f12bbb667ca96
Renat <jeingo20@gmail.com>
2023-02-27 16:07:05 +0300

    fix bug 3

--- src/auth/api/dto/input.confirmation.code.dto.ts
+++ src/auth/api/dto/input.confirmation.code.dto.ts
@@ -1,9 +1,10 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 import { Trim } from '../../../helper/validation-decorators/to.trim.decorator';
 import { EmailConfirmationCodeIsCorrect } from '../../../helper/validation-decorators/email.confirmation.code.is.correct.decorator';
 
 export class InputConfirmationCodeDto {
   @EmailConfirmationCodeIsCorrect()
+  @IsUUID()
   @IsString()
   @IsNotEmpty()
   @Trim()
