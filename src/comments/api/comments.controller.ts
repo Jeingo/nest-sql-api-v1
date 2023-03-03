a888d62057181d919592b7767111bc838e140d65
Renat <jeingo20@gmail.com>
2023-03-03 16:12:21 +0300

    rewrote get by id comment on sql

--- src/comments/api/comments.controller.ts
+++ src/comments/api/comments.controller.ts
@@ -14,19 +14,28 @@ import { OutputCommentDto } from './dto/output.comment.dto';
 import { GetUserGuard } from '../../auth/infrastructure/guards/get.user.guard';
 import { InputCreateCommentDto } from './dto/input.create.comment.dto';
 import { InputUpdateLikeDto } from './dto/input.update.like.dto';
-import { CheckIdAndParseToDBId } from '../../helper/pipes/check.id.validator.pipe';
+import {
+  CheckId,
+  CheckIdAndParseToDBId
+} from '../../helper/pipes/check.id.validator.pipe';
 import { CurrentUser } from '../../helper/get-decorators/current.user.decorator';
-import { CurrentUserType, DbId } from '../../global-types/global.types';
+import {
+  CurrentUserType,
+  DbId,
+  SqlDbId
+} from '../../global-types/global.types';
 import { JwtAuthGuard } from '../../auth/infrastructure/guards/jwt.auth.guard';
 import { CommandBus } from '@nestjs/cqrs';
 import { UpdateCommentCommand } from '../application/use.cases/update.comment.use.case';
 import { RemoveCommentCommand } from '../application/use.cases/remove.comment.use.case';
 import { UpdateLikeStatusInCommentCommand } from '../application/use.cases/update.status.like.in.comment.use.case';
+import { SqlCommentsQueryRepository } from '../infrastructure/sql.comments.query.repository';
 
 @Controller('comments')
 export class CommentsController {
   constructor(
     private readonly commentsQueryRepository: CommentsQueryRepository,
+    private readonly sqlCommentsQueryRepository: SqlCommentsQueryRepository,
     private readonly commandBus: CommandBus
   ) {}
 
@@ -34,10 +43,10 @@ export class CommentsController {
   @HttpCode(HttpStatus.OK)
   @Get(':id')
   async findOne(
-    @Param('id', new CheckIdAndParseToDBId()) id: DbId,
+    @Param('id', new CheckId()) id: SqlDbId,
     @CurrentUser() user: CurrentUserType
   ): Promise<OutputCommentDto> {
-    return this.commentsQueryRepository.getById(id, user);
+    return this.sqlCommentsQueryRepository.getById(id, user);
   }
 
   @UseGuards(JwtAuthGuard)
