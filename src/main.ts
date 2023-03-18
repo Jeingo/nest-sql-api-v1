71c814e71352a874b9d0d7f45b4c17f426ecf231
Renat <jeingo20@gmail.com>
2023-03-18 16:43:03 +0300

    Feature: add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,22 @@ import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './helper/expception-filter/exception.filter';
 import cookieParser from 'cookie-parser';
 import { useContainer } from 'class-validator';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService<IConfigType>);
+
+  const config = new DocumentBuilder()
+    .setTitle('Blogger Api')
+    .setDescription(
+      `Blogger can register and create blogs and posts . Users can create comment and likes`
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.enableCors();
